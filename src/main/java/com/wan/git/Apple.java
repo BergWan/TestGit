954bf99845d28ccd13742c1e72fa25004b3eee2b
wan <1099672633@qq.com>
2019-01-11 13:47:57 +0800

    第三测试  GitTest

--- src/main/java/com/wan/git/Apple.java
+++ src/main/java/com/wan/git/Apple.java
@@ -2,6 +2,6 @@ package com.wan.git;
 
 public class Apple {
 	public static void main(String[] args) {
-		System.out.println("这里TestGit===BergWan ！ second Edit;");
+		System.out.println("这里TestGit===BergWan ！ GitTest Third Edit ");
 	}
 }
