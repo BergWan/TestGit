83822bfbcf915222c93986b9bca6fe5daff02a61
wan <1099672633@qq.com>
2019-01-11 14:02:42 +0800

    最终版

--- src/main/java/com/wan/git/Apple.java
+++ src/main/java/com/wan/git/Apple.java
@@ -2,14 +2,6 @@ package com.wan.git;
 
 public class Apple {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("这里TestGit===BergWan ！ 第四次 ");
-=======
-<<<<<<< HEAD
-		System.out.println("这里是 TestGit2===ToBeNo1  第三次");
-=======
-		System.out.println("这里TestGit===BergWan ！ GitTest Third Edit ");
->>>>>>> branch 'master' of https://github.com/BergWan/TestGit.git
->>>>>>> branch 'master' of https://github.com/BergWan/TestGit.git
 	}
 }
