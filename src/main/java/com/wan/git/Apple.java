78323b9dbf6f21e745ba7e3cddf73b11b0fd1a62
wan <1099672633@qq.com>
2019-01-11 13:38:41 +0800

    测试

--- src/main/java/com/wan/git/Apple.java
+++ src/main/java/com/wan/git/Apple.java
@@ -2,10 +2,6 @@ package com.wan.git;
 
 public class Apple {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("这里TestGit===BergWan ！ second Edit;");
-=======
-		System.out.println("这里是 TestGit2===ToBeNo1 seconf edit");
->>>>>>> branch 'master' of https://github.com/BergWan/TestGit.git
 	}
 }
