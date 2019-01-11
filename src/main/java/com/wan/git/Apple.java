14a6e335894af67d3702c8dd9c4695eeba228bbd
wan <1099672633@qq.com>
2019-01-11 13:06:31 +0800

    第二次修改，用于测试冲突问题

--- src/main/java/com/wan/git/Apple.java
+++ src/main/java/com/wan/git/Apple.java
@@ -2,6 +2,6 @@ package com.wan.git;
 
 public class Apple {
 	public static void main(String[] args) {
-		System.out.println("这里是 TestGit2===ToBeNo1");
+		System.out.println("这里是 TestGit2===ToBeNo1 seconf edit");
 	}
 }
